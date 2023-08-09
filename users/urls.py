0d23be6b8942c23ccd8d47064d1e8524b3e418b3
Polina <polinakiseleva4@gmail.com>
2023-08-09 19:24:23 +0500

    Обновление урлов под верификацию

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import LoginView, LogoutView, RegisterView, ProfileView
+from users.views import RegisterView, VerifyEmailView, VerifyEmailSentView, EmailConfirmedView
+from django.contrib.auth.views import LoginView, LogoutView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path('', LoginView.as_view(), name='login'),
+    path('', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
-    path('profile/', ProfileView.as_view(), name='profile'),
-
+    path('verify_email/<str:uidb64>/<str:token>/', VerifyEmailView.as_view(), name='verify_email'),
+    path('verify_email_sent/', VerifyEmailSentView.as_view(), name='verify_email_sent'),
+    path('email_confirmed/', EmailConfirmedView.as_view(), name='email_confirmed'),
 ]
